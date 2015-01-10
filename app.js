715ea98dfe03c88cd6dce663140c873d84bdc013
Anson Yu Wang <opensource@undozen.com>
2015-01-10 18:03:17 +0800

    load config from home

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+#!/usr/bin/env node
 var http = require('http');
 var multiParty = require('connect-multiparty');
 var fs = require('fs');
 var UPYun = require('./upyun').UPYun;
-var config = require('./config.json');
+var config;
+
+var homeConfigPath = process.env.HOME + '/.dnd-upload-upyun.json'
+try {
+  config = JSON.parse(fs.readFileSync(homeConfigPath));
+} catch (e) {
+  console.log('please place a config file at ' + homeConfigPath +
+  ' like: \n' + fs.readFileSync(__dirname + '/config.sample.json', 'utf-8'));
+  process.exit(0);
+}
 
 var upyun = new UPYun(config.bucketname, config.username, config.password);
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
   if (req.url != '/upload') {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     fs.createReadStream('./index.html').pipe(res);
@@ -17,4 +27,6 @@ http.createServer(function (req, res) {
   }, { headers: { 'Content-Type': req.files.file.type }});
   res.end('ok');
   });
-}).listen(8080);
+}).listen(function () {
+  console.log('server listening: http://127.0.0.1:' + server.address().port);
+});
