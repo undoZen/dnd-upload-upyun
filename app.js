3e6efbc4d48fb985a4c61ff0596e04339662d3b8
Anson Yu Wang <opensource@undozen.com>
2015-05-17 00:18:53 +0800

    support generate uuid v4 file path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var http = require('http');
 var multiParty = require('connect-multiparty');
 var fs = require('fs');
+var uuid = require('uuid');
 var UPYun = require('./upyun').UPYun;
 var config;
 
@@ -16,17 +17,28 @@ try {
 
 var upyun = new UPYun(config.bucketname, config.username, config.password);
 
+var indexSrc = fs.readFileSync(__dirname + '/index.html', 'utf8')
+.replace('__BUCKET_NAME__', config.bucketname)
+.replace('"__GENERATE_PATH__"', config.generatePath ? 'true' : 'false')
+
 var server = http.createServer(function (req, res) {
-  if (req.url != '/upload') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else multiParty()(req, res, function () {
-  console.log(req);
-  upyun.writeFile(req.body.path, fs.readFileSync(req.files.file.path), true, function (err, res) {
+    res.end(indexSrc);
+  } else if (req.url === '/upload') multiParty()(req, res, function () {
+  var filePath = req.body.path;
+  if (config.generatePath || filePath || filePath === '/') filePath = '/' + uuid.v4();
+  console.log('fp', filePath);
+  upyun.writeFile(filePath, fs.readFileSync(req.files.file.path), true, function (err, res) {
     console.log(err||res);
   }, { headers: { 'Content-Type': req.files.file.type }});
-  res.end('ok');
+  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+  res.end(JSON.stringify({filePath: filePath}));
   });
+  else {
+      res.statusCode = 404;
+      res.end('not found');
+  }
 }).listen(function () {
   console.log('server listening: http://127.0.0.1:' + server.address().port);
 });
