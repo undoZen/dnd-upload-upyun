74f0cd6623cd568ed9b2bf3095dac065545e3160
Anson Yu Wang <opensource@undozen.com>
2015-05-17 06:38:01 +0800

    get /upload

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var config;
 
 var homeConfigPath = process.env.HOME + '/.dnd-upload-upyun.json'
 try {
-  config = JSON.parse(fs.readFileSync(homeConfigPath));
+    config = JSON.parse(fs.readFileSync(homeConfigPath));
 } catch (e) {
-  console.log('please place a config file at ' + homeConfigPath +
-  ' like: \n' + fs.readFileSync(__dirname + '/config.sample.json', 'utf-8'));
-  process.exit(0);
+    console.log('please place a config file at ' + homeConfigPath +
+            ' like: \n' + fs.readFileSync(__dirname + '/config.sample.json', 'utf-8'));
+    process.exit(0);
 }
 
 var upyun = new UPYun(config.bucketname, config.username, config.password);
@@ -22,23 +22,23 @@ var indexSrc = fs.readFileSync(__dirname + '/index.html', 'utf8')
 .replace('"__GENERATE_PATH__"', config.generatePath ? 'true' : 'false')
 
 var server = http.createServer(function (req, res) {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(indexSrc);
-  } else if (req.url === '/upload') multiParty()(req, res, function () {
-  var filePath = req.body.path;
-  if (config.generatePath || filePath || filePath === '/') filePath = '/' + uuid.v4();
-  console.log('fp', filePath);
-  upyun.writeFile(filePath, fs.readFileSync(req.files.file.path), true, function (err, res) {
-    console.log(err||res);
-  }, { headers: { 'Content-Type': req.files.file.type }});
-  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-  res.end(JSON.stringify({filePath: filePath}));
-  });
-  else {
-      res.statusCode = 404;
-      res.end('not found');
-  }
+    if (req.method === 'GET' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(indexSrc);
+    } else if (req.url === '/upload') multiParty()(req, res, function () {
+        var filePath = req.body.path;
+        if (config.generatePath || filePath || filePath === '/') filePath = '/' + uuid.v4();
+        console.log('fp', filePath);
+        upyun.writeFile(filePath, fs.readFileSync(req.files.file.path), true, function (err, res) {
+            console.log(err||res);
+        }, { headers: { 'Content-Type': req.files.file.type }});
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        res.end(JSON.stringify({filePath: filePath}));
+    });
+    else {
+        res.statusCode = 404;
+        res.end('not found');
+    }
 }).listen(process.env.PORT || 0, function () {
-  console.log('server listening: http://127.0.0.1:' + server.address().port);
+    console.log('server listening: http://127.0.0.1:' + server.address().port);
 });
